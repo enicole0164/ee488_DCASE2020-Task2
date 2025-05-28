3ffd618269677ceec225f3c550cbe111a0ee26d3
장민주 <minjujang@kaist.ac.kr>
2025-05-28 19:24:18 +0900

    20250528TFgram original & Train cache cleaning

--- trainer.py
+++ trainer.py
@@ -3,8 +3,134 @@ from model.net import TASTgramMFN, SCLTFSTgramMFN, TFSTgramMFN
 from tqdm import tqdm
 from utils import get_accuracy, mixup_data, arcmix_criterion, noisy_arcmix_criterion
 from losses import ASDLoss, ArcMarginProduct
-from torch.amp import autocast  # CHANGE 1: Updated import (was torch.cuda.amp)
+from torch.amp import autocast
 from sklearn import metrics  # CHANGE 2: Added missing import
+
+# class Trainer:
+#     def __init__(self, device, net, mode, m, alpha, epochs=300, class_num=41, lr=1e-4):
+#         self.device = device
+#         self.epochs = epochs
+#         self.alpha = alpha
+
+#         if net == 'SCLTFSTgramMFN':
+#             self.net = SCLTFSTgramMFN(num_classes=class_num, mode=mode, use_arcface=True, m=m).to(self.device)
+#             if mode != 'arcface':
+#                 raise ValueError('SCLTFSTgramMFN only supports arcface mode')
+#         elif net == 'TASTgramMFN':
+#             self.net = TASTgramMFN(num_classes=class_num, mode=mode, use_arcface=True, m=m).to(self.device)
+#         elif net == 'TFSTgramMFN':
+#             self.net = TFSTgramMFN(num_classes=class_num, mode=mode, use_arcface=True, m=m).to(self.device)
+#         else:
+#             raise ValueError('Net should be one of [SCLTFSTgramMFN, TASTgramMFN, TFSTgramMFN]')
+#         print(f'{net} has been selected...')
+        
+#         self.optimizer = torch.optim.AdamW(self.net.parameters(), lr=lr)
+        
+#         self.scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(self.optimizer, T_max=epochs, eta_min=0.1*float(lr))
+#         self.criterion = ASDLoss().to(self.device)
+#         self.test_criterion = ASDLoss(reduction=False).to(self.device)
+#         self.mode = mode
+        
+#         if mode not in ['arcface', 'arcmix', 'noisy_arcmix']:
+#             raise ValueError('Mode should be one of [arcface, arcmix, noisy_arcmix]')
+        
+#         print(f'{mode} mode has been selected...')
+        
+#     def train(self, train_loader, valid_loader, save_path):
+#         num_steps = len(train_loader)
+#         min_val_loss = 1e10
+        
+#         for epoch in tqdm(range(self.epochs), total=self.epochs):
+#             sum_loss = 0.
+#             sum_accuracy = 0.
+            
+#             for _, (x_wavs, x_mels, labels) in tqdm(enumerate(train_loader), total=num_steps):
+#                 self.net.train()
+                
+#                 x_wavs, x_mels, labels = x_wavs.to(self.device), x_mels.to(self.device), labels.to(self.device)
+                
+#                 with autocast('cuda'):
+#                     if self.mode == 'arcface':
+#                         logits, _ = self.net(x_wavs, x_mels, labels)
+#                         loss = self.criterion(logits, labels)
+                    
+#                     elif self.mode == 'noisy_arcmix':
+#                         mixed_x_wavs, mixed_x_mels, y_a, y_b, lam = mixup_data(x_wavs, x_mels, labels, self.device, alpha=self.alpha)
+#                         logits, _ = self.net(mixed_x_wavs, mixed_x_mels, labels)
+#                         loss = noisy_arcmix_criterion(self.criterion, logits, y_a, y_b, lam)
+                    
+#                     elif self.mode == 'arcmix':
+#                         mixed_x_wavs, mixed_x_mels, y_a, y_b, lam = mixup_data(x_wavs, x_mels, labels, self.device, alpha=self.alpha)
+#                         logits, logits_shuffled, _ = self.net(mixed_x_wavs, mixed_x_mels, [y_a, y_b])
+#                         loss = arcmix_criterion(self.criterion, logits, logits_shuffled, y_a, y_b, lam)
+                
+#                 sum_accuracy += get_accuracy(logits, labels)
+                
+#                 self.optimizer.zero_grad()
+#                 loss.backward()
+#                 self.optimizer.step()
+                
+#                 sum_loss += loss.item()
+#             self.scheduler.step()
+                
+#             avg_loss = sum_loss / num_steps
+#             avg_accuracy = sum_accuracy / num_steps
+            
+#             valid_loss, valid_accuracy = self.valid(valid_loader)
+            
+#             if min_val_loss > valid_loss:
+#                 min_val_loss = valid_loss
+#                 lr = self.scheduler.get_last_lr()[0]
+#                 print("model has been saved!")
+#                 print(f'lr: {lr:.7f} | EPOCH: {epoch} | Train_loss: {avg_loss:.5f} | Train_accuracy: {avg_accuracy:.5f} | Valid_loss: {valid_loss:.5f} | Valid_accuracy: {valid_accuracy:.5f}')
+#                 torch.save(self.net.state_dict(), save_path)
+                
+#     def valid(self, valid_loader):
+#         self.net.eval()
+        
+#         num_steps = len(valid_loader)
+#         sum_loss = 0.
+#         sum_accuracy = 0.
+        
+#         for (x_wavs, x_mels, labels) in valid_loader:
+#             x_wavs, x_mels, labels = x_wavs.to(self.device), x_mels.to(self.device), labels.to(self.device)
+#             logits, _ = self.net(x_wavs, x_mels, labels, train=False)
+#             sum_accuracy += get_accuracy(logits, labels)
+#             loss = self.criterion(logits, labels)
+#             sum_loss += loss.item()
+            
+#         avg_loss = sum_loss / num_steps 
+#         avg_accuracy = sum_accuracy / num_steps 
+#         return avg_loss, avg_accuracy
+    
+#     def test(self, test_loader):
+#         self.net.eval()
+        
+#         y_true = []
+#         y_pred = []
+        
+#         sum_accuracy = 0.
+#         with torch.no_grad():
+#             for x_wavs, x_mels, labels, AN_N_labels in test_loader:
+#                 x_wavs, x_mels, labels, AN_N_labels = x_wavs.to(self.device), x_mels.to(self.device), labels.to(self.device), AN_N_labels.to(self.device)
+                
+#                 logits, _ = self.net(x_wavs, x_mels, labels, train=False)
+#                 score = self.test_criterion(logits, labels)
+#                 sum_accuracy += get_accuracy(logits, labels)
+                
+#                 y_pred.extend(score.tolist())
+#                 y_true.extend(AN_N_labels.tolist())
+#         auc = metrics.roc_auc_score(y_true, y_pred)
+#         #pauc = metrics.roc_auc_score(y_true, y_pred, max_fpr=0.1)
+#         return auc, sum_accuracy / len(test_loader)
+
+import torch
+from model.net import TASTgramMFN, SCLTFSTgramMFN, TFSTgramMFN
+from tqdm import tqdm
+from utils import get_accuracy, mixup_data, arcmix_criterion, noisy_arcmix_criterion
+from losses import ASDLoss, ArcMarginProduct
+from torch.amp import autocast
+from sklearn import metrics
 
 class Trainer:
     def __init__(self, device, net, mode, m, alpha, epochs=300, class_num=41, lr=1e-4):
@@ -40,16 +166,20 @@ class Trainer:
         num_steps = len(train_loader)
         min_val_loss = 1e10
         
-        for epoch in tqdm(range(self.epochs), total=self.epochs):
+        # MEMORY FIX 1: Remove double progress bar - no tqdm on epoch loop
+        for epoch in range(self.epochs):
             sum_loss = 0.
             sum_accuracy = 0.
             
-            for _, (x_wavs, x_mels, labels) in tqdm(enumerate(train_loader), total=num_steps):
+            # MEMORY FIX 2: Single progress bar with epoch info
+            for i, (x_wavs, x_mels, labels) in tqdm(enumerate(train_loader), 
+                                                    total=num_steps, 
+                                                    desc=f"Epoch {epoch+1}/{self.epochs}"):
                 self.net.train()
                 
                 x_wavs, x_mels, labels = x_wavs.to(self.device), x_mels.to(self.device), labels.to(self.device)
                 
-                with autocast('cuda'):  # CHANGE 3: Added 'cuda' parameter
+                with autocast('cuda'):
                     if self.mode == 'arcface':
                         logits, _ = self.net(x_wavs, x_mels, labels)
                         loss = self.criterion(logits, labels)
@@ -71,10 +201,18 @@ class Trainer:
                 self.optimizer.step()
                 
                 sum_loss += loss.item()
+                
+                # MEMORY FIX 3: Clear cache every 50 batches during training
+                if (i + 1) % 50 == 0:
+                    torch.cuda.empty_cache()
+                    
             self.scheduler.step()
                 
             avg_loss = sum_loss / num_steps
             avg_accuracy = sum_accuracy / num_steps
+            
+            # MEMORY FIX 4: Clear cache before validation
+            torch.cuda.empty_cache()
             
             valid_loss, valid_accuracy = self.valid(valid_loader)
             
@@ -92,13 +230,18 @@ class Trainer:
         sum_loss = 0.
         sum_accuracy = 0.
         
-        with torch.no_grad():  # CHANGE 4: Added explicit no_grad context
-            for (x_wavs, x_mels, labels) in valid_loader:
+        # MEMORY FIX 5: Add torch.no_grad() to prevent gradient computation in validation
+        with torch.no_grad():
+            for i, (x_wavs, x_mels, labels) in enumerate(valid_loader):
                 x_wavs, x_mels, labels = x_wavs.to(self.device), x_mels.to(self.device), labels.to(self.device)
                 logits, _ = self.net(x_wavs, x_mels, labels, train=False)
                 sum_accuracy += get_accuracy(logits, labels)
                 loss = self.criterion(logits, labels)
                 sum_loss += loss.item()
+                
+                # MEMORY FIX 6: Clear cache every 10 validation batches
+                if (i + 1) % 10 == 0:
+                    torch.cuda.empty_cache()
                 
         avg_loss = sum_loss / num_steps 
         avg_accuracy = sum_accuracy / num_steps 
@@ -112,7 +255,7 @@ class Trainer:
         
         sum_accuracy = 0.
         with torch.no_grad():
-            for x_wavs, x_mels, labels, AN_N_labels in test_loader:
+            for i, (x_wavs, x_mels, labels, AN_N_labels) in enumerate(test_loader):
                 x_wavs, x_mels, labels, AN_N_labels = x_wavs.to(self.device), x_mels.to(self.device), labels.to(self.device), AN_N_labels.to(self.device)
                 
                 logits, _ = self.net(x_wavs, x_mels, labels, train=False)
@@ -121,6 +264,10 @@ class Trainer:
                 
                 y_pred.extend(score.tolist())
                 y_true.extend(AN_N_labels.tolist())
+                
+                # MEMORY FIX 7: Clear cache every 20 test batches
+                if (i + 1) % 20 == 0:
+                    torch.cuda.empty_cache()
+                    
         auc = metrics.roc_auc_score(y_true, y_pred)
-        #pauc = metrics.roc_auc_score(y_true, y_pred, max_fpr=0.1)
         return auc, sum_accuracy / len(test_loader)
